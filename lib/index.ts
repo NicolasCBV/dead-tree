7ebcf114ed7c7fb0e09bbc62060d4e0654089803
NicolasCBV <nicolascbv700@gmail.com>
2024-08-18 00:40:34 -0300

    fix: add number comparator

--- lib/index.ts
+++ lib/index.ts
@@ -2,4 +2,5 @@ export type { IComparator } from './types/IComparator';
 export type { IAVLTree } from './trees/AVLTree';
 
 export { AVLTree } from './trees/AVLTree';
+export { NumberComparator } from './comparators/NumberComparator';
 export { TreeNode } from './trees/nodes/TreeNode';
